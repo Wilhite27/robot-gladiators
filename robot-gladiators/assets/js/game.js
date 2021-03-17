e25a75164dfd9ce0ae7b6d1a8628ac00e9b46e4d
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-03-17 00:42:33 -0500

    3.1.5 push

--- robot-gladiators/assets/js/game.js
+++ robot-gladiators/assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log("Your Robot's name is " + playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+function fight() {
+    window.alert("The fight has begun!");
+  }
+//   fight();
